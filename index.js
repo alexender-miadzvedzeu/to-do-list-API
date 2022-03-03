aaa1d3b57de36b3c07375fa2cac55ec0d9ff3260
Александр Медведев <medved3993@gmail.com>
2022-03-03 22:43:46 +0300

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
+const cors = require('cors');
 const PORT = 7777;
 const path = require('path');
 
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
